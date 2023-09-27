073df16b3d2ac5752180a11c14f78cdf4ec29466
syedamanhussain <121918194+syedamanhussain@users.noreply.github.com>
2023-09-27 05:04:40 -0700

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
+      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6570374387:AAEbb8O7StFdzGBrawhc1vqvcDBtLdmdZ7E")
+      API_ID = int(os.environ.get("APP_ID", "26852980"))
+      API_HASH = os.environ.get("API_HASH", "b69a1ec7ff063aa1a4d1955238ad47c9")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
+      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "bookuploadingbot")
       ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
       DB_URL = os.environ.get("DATABASE_URL", "")
